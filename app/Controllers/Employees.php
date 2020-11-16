4c4a373c79eac54589e288cd6d79b825a4087122
33950 <33950@DESKTOP-CM1MCPP>
2020-11-16 09:15:23 +0800

    change the baseURL in App.php and add template folder with two file bottom and top. Then add code in Employees.php to display data by view. Add json file in data folder and change Ds.php to get data from json file

--- app/Controllers/Employees.php
+++ app/Controllers/Employees.php
@@ -12,9 +12,40 @@ namespace App\Controllers;
          //get a template parser
          $parser = \Config\Services::parser();
          //tell it about the substitions
-         return $parser->setData(['records' => $records])
-                 // and have it render the template with those
-                 ->render('employeeslist');
+//         return $parser->setData(['records' => $records])
+//                 // and have it render the template with those
+//                 ->render('employeeslist');
+         
+         $table = new \CodeIgniter\View\Table();
+
+         $headings = $employees->fields;
+         $displayHeadings = array_slice($headings, 1, 2);
+         $table->setHeading(array_map('ucfirst', $displayHeadings));
+         
+         foreach ($records as $record) {
+             $nameLink = anchor("employees/showme/$record->id",$record->name);
+             $table->addRow($nameLink,$record->nationality);
+         }
+         
+         $template = [
+             'table_open' => '<table cellpadding="10px">',
+             'cell_start' => '<td style="border: 5px solid #FF0000;">',
+             'row_alt_start' => '<tr style="background-color:yellow">',
+             ];
+         $table->setTemplate($template);
+         
+         $fields = [
+             'title' => 'Deathing Standing',
+             'heading' => 'Deathing Standing',
+             'footer' => 'Liu Chang'
+             ];
+          return $parser->setData($fields)
+                         ->render('templates\top') .
+                  $table->generate() .
+                  $parser->setData($fields)
+                         ->render('templates\bottom');
+
+         return $table->generate(); 
      }
      
      public function showme($id)
@@ -27,8 +58,42 @@ namespace App\Controllers;
          // get a template parser     
          $parser = \Config\Services::parser();     
          // tell it about the substitions     
-         return $parser->setData($record)         
-                    // and have it render the template with those         
-                    ->render('oneemployee');
+//         return $parser->setData($record)         
+//                    // and have it render the template with those         
+//                    ->render('oneemployee');
+         $table = new \CodeIgniter\View\Table();
+
+         $headings = $employees->fields;
+//         $table->setHeading(array_map('ucfirst', $displayHeadings));
+         
+        $table->addRow($headings[0],$record['id']);
+        $table->addRow($headings[1],$record['name']);
+        $table->addRow($headings[2],$record['country']);
+        $table->addRow($headings[3],"<img src=\"/image/".$record['image']."\"/>");
+        $table->addRow($headings[4],$record['occupation']);
+        $table->addRow($headings[5],$record['notablework']);
+        $table->addRow($headings[6],$record['nationality']);
+        $table->addRow($headings[7],$record['born']);
+        
+      
+         $template = [
+             'table_open' => '<table cellpadding="10px">',
+             'cell_start' => '<td style="border: 5px solid #FF0000;">',
+             'row_alt_start' => '<tr style="background-color:#green">',
+             ];
+         $table->setTemplate($template);
+         
+         $fields = [
+             'title' => 'Deathing Standing',
+             'heading' => 'Deathing Standing',
+             'footer' => 'Liu Chang'
+             ];
+          return $parser->setData($fields)
+                         ->render('templates\top') .
+                  $table->generate() .
+                  $parser->setData($fields)
+                         ->render('templates\bottom');
+
+         return $table->generate(); 
      }
  }
--- app/Models/Ds.php
+++ app/Models/Ds.php
@@ -1,86 +1,89 @@
 <?php
 namespace App\Models;
-
+use App\Models\Simple\JSONModel;
 /*
  * Mock travel destination data.
  * Note that we don't have to extend CodeIgniter's model for now
  */
 
-class Ds {
+class Ds extends JSONModel{
+    protected $origin = WRITEPATH . 'data/dsData.json'; 
+    protected $keyField = 'id'; 
+    protected $validationRules = [];
 
     //mock data : an array of records
-    protected $data = [
-        '1' => [
-            'id' => 1,
-            'name' => 'Hideo Kojima',
-            'country' => 'Japan',
-            'image' => 'kojima.jpg',
-            'occupation' => 'video game designer',
-            'notablework' => 'Metal Gear, Snatcher, Zone of the Enders, Death Stranding',
-            'nationality' => 'Japanese',
-            'born' => 'August 24, 1963',
-        ],
-        '2' => [
-            'id' => 2,
-            'name' => 'Yoji Shinkawa',
-            'country' => 'Japan',
-            'image' => 'shinkawa.jpg',
-            'occupation' => 'illustrator',
-            'notablework' => 'Metal Gear, Call of Duty: Black Ops Ⅲ, Zone of the Enders, Death Stranding',
-            'nationality' => 'Japanese',
-            'born' => 'December 25, 1971',
-        ],
-        '3' => [
-            'id' => 3,
-            'name' => 'Norman Mark Reedus',
-            'country' => 'America',
-            'image' => 'norman.jpg',
-            'occupation' => 'voice and action capture provider',
-            'notablework' => 'The Walking Dead, Death Stranding',
-            'nationality' => 'American',
-            'born' => 'January 6, 1969',
-        ],
-        '4' => [
-            'id' => 4,
-            'name' => 'Mads Dittmann Mikkelsen',
-            'country' => 'Denmark',
-            'image' => 'mads.jpg',
-            'occupation' => 'voice and action capture provider',
-            'notablework' => 'King Arthur, Chaos Walking, Death Stranding',
-            'nationality' => 'Dane',
-            'born' => 'November 22, 1965',
-        ],
-        '5' => [
-            'id' => 5,
-            'name' => 'Léa Seydoux',
-            'country' => 'France',
-            'image' => 'seydoux.jpg',
-            'occupation' => 'voice and action capture provider',
-            'notablework' => 'The Beautiful Person, No Time to Die, Death Stranding',
-            'nationality' => 'French',
-            'born' => 'July 1, 1985',
-        ],
-        '6' => [
-            'id' => 6,
-            'name' => 'Margaret Qualley',
-            'country' => 'America',
-            'image' => 'margaret.jpg',
-            'occupation' => 'voice and action capture provider',
-            'notablework' => 'Adam, Seberg, Death Stranding',
-            'nationality' => 'American',
-            'born' => 'October 23, 1994',
-        ],
-    ];
-
-    public function findAll() {
-        return $this->data;
-    }
-
-    public function find($id = null) {
-        if (!empty($id) && isset($this->data[$id])) {
-            return $this->data[$id];
-        }
-        return null;
-    }
+//    protected $data = [
+//        '1' => [
+//            'id' => 1,
+//            'name' => 'Hideo Kojima',
+//            'country' => 'Japan',
+//            'image' => 'kojima.jpg',
+//            'occupation' => 'video game designer',
+//            'notablework' => 'Metal Gear, Snatcher, Zone of the Enders, Death Stranding',
+//            'nationality' => 'Japanese',
+//            'born' => 'August 24, 1963',
+//        ],
+//        '2' => [
+//            'id' => 2,
+//            'name' => 'Yoji Shinkawa',
+//            'country' => 'Japan',
+//            'image' => 'shinkawa.jpg',
+//            'occupation' => 'illustrator',
+//            'notablework' => 'Metal Gear, Call of Duty: Black Ops Ⅲ, Zone of the Enders, Death Stranding',
+//            'nationality' => 'Japanese',
+//            'born' => 'December 25, 1971',
+//        ],
+//        '3' => [
+//            'id' => 3,
+//            'name' => 'Norman Mark Reedus',
+//            'country' => 'America',
+//            'image' => 'norman.jpg',
+//            'occupation' => 'voice and action capture provider',
+//            'notablework' => 'The Walking Dead, Death Stranding',
+//            'nationality' => 'American',
+//            'born' => 'January 6, 1969',
+//        ],
+//        '4' => [
+//            'id' => 4,
+//            'name' => 'Mads Dittmann Mikkelsen',
+//            'country' => 'Denmark',
+//            'image' => 'mads.jpg',
+//            'occupation' => 'voice and action capture provider',
+//            'notablework' => 'King Arthur, Chaos Walking, Death Stranding',
+//            'nationality' => 'Dane',
+//            'born' => 'November 22, 1965',
+//        ],
+//        '5' => [
+//            'id' => 5,
+//            'name' => 'Léa Seydoux',
+//            'country' => 'France',
+//            'image' => 'seydoux.jpg',
+//            'occupation' => 'voice and action capture provider',
+//            'notablework' => 'The Beautiful Person, No Time to Die, Death Stranding',
+//            'nationality' => 'French',
+//            'born' => 'July 1, 1985',
+//        ],
+//        '6' => [
+//            'id' => 6,
+//            'name' => 'Margaret Qualley',
+//            'country' => 'America',
+//            'image' => 'margaret.jpg',
+//            'occupation' => 'voice and action capture provider',
+//            'notablework' => 'Adam, Seberg, Death Stranding',
+//            'nationality' => 'American',
+//            'born' => 'October 23, 1994',
+//        ],
+//    ];
+//
+//    public function findAll() {
+//        return $this->data;
+//    }
+//
+//    public function find($id = null) {
+//        if (!empty($id) && isset($this->data[$id])) {
+//            return $this->data[$id];
+//        }
+//        return null;
+//    }
 
 }
